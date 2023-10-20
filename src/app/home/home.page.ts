2d6b90d6b77a9f3295ff8b50f4a6d10992a6c0ae
Romi Julianto <romyjulians@gmail.com>
2023-10-21 00:31:14 +0700

    feat: develop ui homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todoList = [
+    {
+      itemName: 'Coding',
+      itemDueDate: '10-23-23',
+      itemPriority: 'high',
+      itemCategory: 'Work',
+    },
+    {
+      itemName: 'Design',
+      itemDueDate: '10-23-23',
+      itemPriority: 'low',
+      itemCategory: 'Work',
+    },
+    {
+      itemName: 'Shopping',
+      itemDueDate: '10-25-23',
+      itemPriority: 'middle',
+      itemCategory: 'Personal',
+    },
+    {
+      itemName: 'Learning',
+      itemDueDate: '10-26-23',
+      itemPriority: 'middle',
+      itemCategory: 'Personal',
+    },
+  ];
+
+  today: number = Date.now();
 
   constructor() {}
-
 }
